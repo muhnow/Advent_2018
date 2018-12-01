c5d560d63a0aafadbbbe00264cc173fe8bc5e393
Ethan Lawton <lawtonethan2@gmail.com>
2018-12-01 01:58:56 -0500

    Day 1 complete

--- Day_1/solution.py
+++ Day_1/solution.py
@@ -1,1 +1,15 @@
-## something else to initiate another commit 
+import itertools
+# part 1
+input_data = [int(x) for x in open("input.txt").readlines()]
+print(sum(input_data))
+
+# part 2
+frequency = 0
+frequency_set = set([0])
+for num in itertools.cycle(input_data):
+    frequency += num
+    if frequency in frequency_set:
+        print(frequency)
+        break
+    frequency_set.add(frequency)
+
