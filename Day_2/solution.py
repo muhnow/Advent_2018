3a3c3fa33e2d9694468fef64514ae807bf350502
Ethan Lawton <lawtonethan2@gmail.com>
2018-12-02 15:17:21 -0500

    slight pruning on day 2 solution

--- Day_2/solution.py
+++ Day_2/solution.py
@@ -3,33 +3,21 @@ import difflib
 input_data = [x.rstrip() for x in open("input.txt").readlines()]
 
 def part1():
-	two_count = 0;
-	three_count = 0;
-	has_two = False
-	has_three = False
+	two_count = three_count = 0
+	has_two = has_three = False
 
 	for data in input_data:
 		for character in data:
 			count = data.count(character)
-			if(count == 2):
-				has_two = True
-			if(count == 3):
-				has_three = True
-
-		if(has_two):
-			two_count += 1
-		if(has_three):
-			three_count += 1	
-
-		has_two = False
-		has_three = False
 			
+			has_two	= True if count == 2 else has_two
+			has_three = True if count == 3 else has_three
+
+		two_count += 1 if has_two else 0	
+		three_count += 1 if has_three else 0
+		has_two = has_three = False
 
 	print(two_count * three_count)
-
-
-part1()
-
 
 def part2():
 	correct_block = ""
@@ -38,14 +26,12 @@ def part2():
 		for y in input_data[outer+1:]:
 			diff_indices = [i for i in range(len(x)) if x[i] != y[i]]
 			if(len(diff_indices) == 1):
-				print(x, y)
 				index = diff_indices[0]
 
 				correct_block = x[:index] + x[index+1:]
 				break
 
-
 	print(correct_block)
 
-
+part1()
 part2()
